1c1976944e5005277906d03b39aace60fe8fbec5
abdulla-En <abdulla4112018@gmail.com>
2026-03-10 02:32:46 +0200

    MONO and VIGENERE done , TRANS soon!

--- src/main/java/com/encryption/strategies/VigenereCipher.java
+++ src/main/java/com/encryption/strategies/VigenereCipher.java
@@ -2,25 +2,55 @@ package com.encryption.strategies;
 
 public class VigenereCipher implements EncryptionStrategy
 {
-    private String keyword;
+    private String key;
 
     public VigenereCipher(String keyword) {
 
         if(!keyword.matches("[a-zA-Z]+"))
             throw new IllegalArgumentException("Key must contain only letters");
-
-        this.keyword = keyword.toLowerCase();
+        this.key = keyword.toLowerCase();
     }
     @Override
     public String encrypt(String text) {
-        // bokra b3on allah
-        return "";
+        StringBuilder sb  = new StringBuilder();
+        int keyLen = key.length();
+        int keyindex = 0;
+        for(char c : text.toCharArray())
+        {
+            if(c>='a'&&c<='z')
+            {
+                int shift = key.charAt(keyindex%keyLen)-'a';
+                char enc = (char)((c-'a'+shift) % 26 +'a');
+                sb.append(enc);
+                keyindex++;
+            }
+            else
+                sb.append(c);
+        }
+        return sb.toString();
     }
     @Override
     public String decrypt(String decryptedText)
     {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        int keyLen = key.length();
+        int keyindex = 0;
+        for(char c :decryptedText.toCharArray())
+        {
+            if(c>='a'&&c<='z')
+            {
+                int shift = key.charAt(keyindex%keyLen)-'a';
+                char dec = (char)((c-'a'-shift +26)% 26 +'a');
+                sb.append(dec);
+                keyindex++;
+            }
+            else
+                sb.append(c);
+        }
+        return sb.toString();
     }
+
+
     @Override
-    public String getName() {return "VIGNERE";}
+    public String getName() {return "VIGENERE";}
 }
