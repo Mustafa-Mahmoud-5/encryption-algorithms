522d6313913c94ecfc20124c24404fb0b9f3d0c2
Mustafa-Mahmoud-5 <mustafamahmoud4004@gmail.com>
2026-03-10 07:50:55 +0200

    feat: implemented mangler func

--- src/main/java/com/encryption/des/ManglerFunction.java
+++ src/main/java/com/encryption/des/ManglerFunction.java
@@ -1,13 +1,47 @@
 package com.encryption.des;
 
+import com.encryption.des.constants.PermutationMatrices;
+import com.encryption.des.constants.SBoxes;
+import com.encryption.des.utils.BitsUtils;
+import com.encryption.des.utils.Permutator;
+
 public class ManglerFunction {
     public String apply(String text, String roundKey) {
         // 1- expansion to 48 bits
+        text = Permutator.permutate(PermutationMatrices.EXPANSION_TABLE, text);
+
         // 2- XOR with round key
-        // 3- for each 6 bits, apply keySubstitution using 6 Sboxes -> 32 bits
+        text = BitsUtils.xorStrings(text, roundKey);
+
+        // 3- get back to 32 bits
+        text = applySBoxesKeySubstitution(text);
+
         // 4- Transposition P-box
+        text = Permutator.permutate(PermutationMatrices.P_BOX, text);
         // return res
 
-        return "";
+        return text;
+    }
+
+    private String applySBoxesKeySubstitution(String text) {
+        StringBuilder res = new StringBuilder();
+        int p = 0;
+        for(int i = 0; i < 8; i++) {
+            String sub = text.substring(p, p+6);
+            String rowBit = (sub.charAt(0) + "" + sub.charAt(sub.length() - 1));
+            String colBit =   sub.substring(1, sub.length() - 1);
+
+
+
+            int row = BitsUtils.bitToInt(rowBit);
+            int col = BitsUtils.bitToInt(colBit);
+
+            int sBoxResInt = SBoxes.S_BOXES[i][row][col];
+            String SBoxRes= BitsUtils.intToBit(sBoxResInt);
+
+            res.append(SBoxRes);
+            p+=6;
+        }
+        return res.toString();
     }
 }
